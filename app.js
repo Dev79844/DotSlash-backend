db7bd40071b5c736fc0e33d1a932ada26af164fa
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-02-18 17:38:48 +0530

    add: cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 require('dotenv').config()
 const user = require('./routes/researcher')
 const grant = require('./routes/grant')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('tiny'))
